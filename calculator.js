175c305c187f42579a49abd631c027b5c683d4da
Brett Stine <r.brettstine@gmail.com>
2023-06-03 13:11:16 -0400

    Implement Class to create Calculator object
    Class has method calculate that takes a string input of a mathematical
    expression and returns the result.

---
+++ calculator.js
@@ -0,0 +1,19 @@
+function Calculator() {
+
+    this.operators = {
+        "+": (a,b) => a + b,
+        "-": (a,b) => a - b,
+        "*": (a,b) => a * b,
+        "/": (a,b) => a / b
+    };
+
+    this.calculate = function(expression) {
+
+        let split = str.split(' '),
+            a = +split[0],
+            op = split[1],
+            b = +split[2];
+
+        return this.operators[op](a,b);
+    };
+};
