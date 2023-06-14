730b04b12ab6baa7038c4c190f8e0b89ff3c0c9a
Brett Stine <r.brettstine@gmail.com>
2023-06-14 17:15:27 -0400

    Backspace is now usable
    also had to update CSS to prevent output from collapsing after Backspace
    to null value.

--- calculator.js
+++ calculator.js
@@ -24,6 +24,20 @@ function Calculator() {
         return Math.round(num * Math.pow(10, 8)) / Math.pow(10, 8);
     }
 
+    this.backspace = function() {
+        if (this.a !== null && this.b === null) {
+            this.a = this.a.toString();
+            this.a = this.a.slice(0,-1);
+            if (this.a === "") this.a = null;
+            this.updateOutput(this.a);
+        } else if (this.b !== null) {
+            this.b = this.b.toString();
+            this.b = this.b.slice(0,-1);
+            if (this.b === "") this.b = null;
+            this.updateOutput(this.b);
+        }
+    }
+
     this.operators = {
         "+": (a,b) => a + b,
         "-": (a,b) => a - b,
@@ -51,6 +65,8 @@ function Calculator() {
             this.op = null;
             this.updateOutput(0);
             return;
+        } else if (buttonStr === "backspace"){
+            this.backspace();
         } else if (buttonStr === "=") { // equals pressed
             if (this.a !== null && this.op !== null && this.b !== null) {
                 this.calculate();
@@ -59,9 +75,10 @@ function Calculator() {
         } else if (buttonStr in this.operators) { //operator pressed
             if (this.a !== null && this.op !== null && this.b !== null) {
                 this.calculate();
+            } else {
+                this.op = buttonStr;
+                return;
             }
-            this.op = buttonStr;
-            return;
         } else if (this.a ===  null || this.op === null) {
             this.a = this.updateNumber(this.a, buttonStr);
             return;
@@ -79,5 +96,6 @@ function Calculator() {
         this.a = this.roundTo8DecimalPlaces(this.operators[this.op](+this.a,+this.b));
         this.updateOutput(this.a);
         this.b = null;
+        this.op = null;
     };
 };
